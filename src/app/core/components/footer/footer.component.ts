b16169a3f14c2df84bdb69dce6fb582c68cc8270
Deepan Ignaatious <31543067+deepanigi@users.noreply.github.com>
2019-01-25 18:23:01 +0100

    app

---
+++ src/app/core/components/footer/footer.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  template: `
+    <footer class="footer">
+    <div class="container">
+    <div class="content has-text-centered">
+   
+    </div>
+    </div>
+    </footer>
+  `,
+  styles: []
+})
+export class FooterComponent implements OnInit {
+  constructor() {}
+  ngOnInit() {}
+}
